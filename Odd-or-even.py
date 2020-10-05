09c31951353f967bee91c2c5db31d1abb4925d85
Akshima-Ghai <58785998+Akshima-Ghai@users.noreply.github.com>
2020-10-06 00:30:12 +0530

    Update Odd-or-even.py

--- Odd-or-even.py
+++ Odd-or-even.py
@@ -12,11 +12,11 @@ lower_num=int(input("Enter the first number for range: "))
 uppper_num=int(input("Enter the second number for range: "))
 
 print("Display the even numbers between two numbers are: ")
-for i in range(Lower_Num,Upper_Num+1):
+for i in range(lower_num,upper_num+1):
     if(i%2==0):
         print(i)
 
 print("Display the odd numbers between two numbers are: ")
-for i in range(Lower_Num,Upper_Num+1):
+for i in range(lower_num,upper_num+1):
     if(i%2==1):
         print(i)    
